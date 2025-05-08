8307c4f75c17b59cc334048c2da0fa4963a0b2aa
Liikemies-glitch <mikki@goodside.fi>
2025-05-08 11:17:52 +0300

    Fix redirect path for palvelumuotoilu-kuntien article

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,8 @@ export default defineConfig({
   redirects: {
     '/in-english/': '/en/',
     '/fi/in-english/': '/en/',
-    '/palvelumuotoilu-kuntien-ja-kaupunkien-kehittamisen-tukena/': '/palvelumuotoilu-kuntien-kehittamisessa/'
+    '/palvelumuotoilu-kuntien-ja-kaupunkien-kehittamisen-tukena/': '/fi/palvelumuotoilu-kuntien-kehittamisessa/',
+    '/fi/palvelumuotoilu-kuntien-ja-kaupunkien-kehittamisen-tukena/': '/fi/palvelumuotoilu-kuntien-kehittamisessa/'
   },
   vite: {
     plugins: [tailwindcss()]
